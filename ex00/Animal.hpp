d0ad986f5d9013b5e15586497c3811dd63047090
killinq-joke <zakadmaster@gmail.com>
2021-11-06 15:20:42 +0100

    ADDED: Animal

---
+++ ex00/Animal.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mout <mout@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/06 15:13:45 by mout              #+#    #+#             */
+/*   Updated: 2021/11/06 15:16:56 by mout             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+# include <iostream>
+
+class Animal
+{
+public:
+    Animal(void);
+    ~Animal(void);
+protected:
+    std::string type;
+};
+
+Animal::Animal(/* args */)
+{
+}
+
+Animal::~Animal()
+{
+}
+
+
+#endif
