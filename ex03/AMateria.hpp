4b0535ed6ecb6783d362dd41f364aaa82f80f6ef
Zakaria Touzri <ztouzri@c1r3p8.42nice.fr>
2021-11-12 08:53:51 +0100

    ADDED: main.cpp

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -5,36 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/10 15:10:14 by ztouzri           #+#    #+#             */
-/*   Updated: 2021/11/11 09:06:47 by ztouzri          ###   ########.fr       */
+/*   Created: 2021/11/12 08:53:37 by ztouzri           #+#    #+#             */
+/*   Updated: 2021/11/12 08:53:38 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef AMATERIA_HPP
-# define AMATERIA_HPP
-# include <iostream>
-# include "ICharacter.hpp"
-
-class ICharacter;
-
-class	AMateria
-{
-
-public:
-		AMateria(void);
-		AMateria(std::string const & type);
-		AMateria(AMateria const & a1);
-		// virtual AMateria	operator=(AMateria const & a1);
-		virtual ~AMateria(void);
-		
-		std::string const & getType() const; //Returns the materia type
-		void		setType(std::string const & type);
-		virtual AMateria* clone() const = 0;
-		virtual void use(ICharacter& target);
-
-protected:
-	std::string _type;
-
-};
-
-#endif
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -5,17 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ztouzri <ztouzri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/06 15:12:42 by mout              #+#    #+#             */
-/*   Updated: 2021/11/10 17:24:40 by ztouzri          ###   ########.fr       */
+/*   Created: 2021/11/12 08:49:41 by ztouzri           #+#    #+#             */
+/*   Updated: 2021/11/12 08:53:25 by ztouzri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Character.hpp"
-#include "Materia.hpp"
+#include "Cure.hpp"
+#include "Ice.hpp"
 
 int main()
 {
-	AMateria *ice = new Materia();
-	delete ice;
-	return (0);
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+
+	ICharacter* me = new Character("me");
+
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+
+	ICharacter* bob = new Character("bob");
+
+	me->use(0, *bob);
+	me->use(1, *bob);
+
+	delete bob;
+	delete me;
+	delete src;
+
+	return 0;
 }
